a3facb52cd94dd67f089e29ed2726ecdbcd01088
NotTheRightGuy <janmejaychatterjee@gmail.com>
2022-11-11 10:19:51 +0530

    Added details

--- auiqm/src/components/items.jsx
+++ auiqm/src/components/items.jsx
@@ -12,6 +12,9 @@ function Item({ image, title, price, url, rating }) {
                 <div className="item-title">{title}</div>
                 <div className="item-price">{price}</div>
             </div>
+            <div>
+                <a href="\src\itemDetails.html">Click to Open Details</a>
+            </div>
         </div>
     );
 }
